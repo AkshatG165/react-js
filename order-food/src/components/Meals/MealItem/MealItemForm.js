76e77a5ebfdf00a72d3311bbad71a7fba87935f0
AkshatG165 <guptaakshat3011@gmail.com>
2023-10-19 01:31:24 +0530

    added createPortal functionality for modal

--- order-food/src/components/Meals/MealItem/MealItemForm.js
+++ order-food/src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,17 @@
 import classes from './MealItemForm.module.css';
 
 function MealItemForm() {
+  const onChange = (e) => {};
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('Hi');
+  };
+
   return (
-    <form className={classes['form']}>
+    <form className={classes['form']} onSubmit={onSubmit}>
       <div>
         <label>Amount</label>
-        <input type="number" defaultValue="1" />
+        <input type="number" defaultValue="1" min="1" onChange={onChange} />
       </div>
       <button type="submit">+Add</button>
     </form>
--- order-food/src/components/UI/Modal/Modal.js
+++ order-food/src/components/UI/Modal/Modal.js
@@ -1,12 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Cart from '../../Cart/Cart';
 import Card from '../Card/Card';
 import classes from './Modal.module.css';
 
-function Modal() {
+const Backdrop = () => <div className={`${classes['backdrop']}`} />;
+const Overlay = () => {
   return (
     <Card className={`${classes['modal']}`}>
       <Cart />
     </Card>
+  );
+};
+
+function Modal() {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <Overlay />,
+        document.getElementById('overlay-root')
+      )}
+    </React.Fragment>
   );
 }
 
